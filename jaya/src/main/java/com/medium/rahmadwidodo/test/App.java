faf23a33c5abc00a4d8d6df0add77e07fbd07aea
istjogjadeveloper1 <ist.yogyakarta.developer1@gmail.com>
2019-07-02 17:26:07 +0700

    rapikan

---
+++ jaya/src/main/java/com/medium/rahmadwidodo/test/App.java
@@ -0,0 +1,13 @@
+package com.medium.rahmadwidodo.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
